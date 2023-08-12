afc3ca88f0cf6fb479ed87852f8b1b341c0fc506
Yedam101 <109948144+Yedam101@users.noreply.github.com>
2023-08-12 11:15:03 +0900

    chapter 07

--- pyburger/config/views.py
+++ pyburger/config/views.py
@@ -15,6 +15,22 @@ def burger_list(request):
     }
     return render(request, "burger_list.html", context)
 
+def burger_search(request):
+    print(request.GET)
+    keyword = request.GET.get('url_keyword')
+
+    if keyword is not None:
+        burgers = Burger.objects.filter(name__contains=keyword)
+    else:
+        burgers = Burger.objects.none()
+
+    context = {
+        'burgers':burgers,
+    }
+
+    return render(request, "burger_search.html", context)
+
+
 
 ''' v1
 def main(request):
